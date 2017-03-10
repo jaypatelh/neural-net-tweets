bddce184b61b4e20d651e7ddc217d0b72c7a121d
Jay H Patel <jaypatelh@gmail.com>
2017-03-09 19:05:52 -0800

    Convert labels into one-hot vector style np arrays

--- generate-labels/merge-labels.py
+++ generate-labels/merge-labels.py
@@ -2,6 +2,7 @@ import argparse
 import pickle
 import csv
 import glob
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-pl", "--paid")
@@ -11,11 +12,13 @@ args = parser.parse_args()
 
 matching_labels = {
     'other_relevant_information': 'other_useful_information',
-    'infrastructure_and_utilities': 'infrastructure_and_utilities_damage'
+    'infrastructure_and_utilities': 'infrastructure_and_utilities_damage',
+    'money': 'donation_needs_or_offers_or_volunteering_services'
 }
 
 print "Loading labels..."
 labels = dict()
+unique = set()
 
 if args.paid is not None:
     with open(args.paid, "rU") as labels_file:
@@ -27,6 +30,7 @@ if args.paid is not None:
             if tweetid not in labels:
                 labels[tweetid] = {}
             labels[tweetid]["paid"] = row[0]
+            unique.add(row[0])
 
 if args.volunteer is not None:
     with open(args.volunteer, "rU") as labels_file:
@@ -39,10 +43,25 @@ if args.volunteer is not None:
                 labels[tweetid] = {}
             label = '_'.join(row[2].replace(',', '').split()).lower()
             labels[tweetid]["volunteer"] = label if label not in matching_labels else matching_labels[label]
+            unique.add(labels[tweetid]["volunteer"])
 
 print "Done loading labels."
 
+print "Converting labels into one-hot vectors"
+unique = list(unique)
+num_unique = len(unique)
+labels_vectors = dict()
+for tweetid in labels:
+    vec = np.zeros(num_unique)
+    label = labels[tweetid]
+    if "paid" in label:
+        label = label["paid"]
+    else:
+        label = label["volunteer"]
+    vec[unique.index(label)] = 1.0
+    labels_vectors[tweetid] = vec
+
 with open(args.output, 'wb') as output_file:
-    pickle.dump(labels, output_file, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(labels_vectors, output_file, protocol=pickle.HIGHEST_PROTOCOL)
 
 print "Wrote labels out to", args.output
